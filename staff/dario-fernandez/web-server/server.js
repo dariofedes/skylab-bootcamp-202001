d13f7825ab7a87df29d00f3328fa26fd934a57dd
Darío Fernández <dariofedes@protonmail.com>
2020-02-19 17:48:59 +0100

    express first version

--- staff/dario-fernandez/web-server/server.js
+++ staff/dario-fernandez/web-server/server.js
@@ -1,18 +1,9 @@
-const http = require('http')
-const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
 
-const server = http.createServer((request, response) => {
-    const rs = fs.createReadStream(request.url === '/' ? './index.html' : `.${request.url}.html`)
 
-    rs.on('error', () => {
-        response.writeHead(404, {  'Content-Type': 'text/html'  })
-        response.end('<h1>Not found</h1>')
-    })
-        
-    rs.on('data', chunk => {
-        response.writeHead(200, {  'Content-Type': 'text/html'  })
-        response.end(chunk.toString())
-    })
-})
 
-server.listen(8080)
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.listen('8080')
