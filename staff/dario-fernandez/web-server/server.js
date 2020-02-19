852fe6f79a0b1b1b512987a2c39fe443141604b5
Darío Fernández <dariofedes@protonmail.com>
2020-02-19 11:28:52 +0100

    add http server

--- staff/dario-fernandez/web-server/server.js
+++ staff/dario-fernandez/web-server/server.js
@@ -1,27 +1,17 @@
-const net = require('net')
+const http = require('http')
 const fs = require('fs')
 
-const server = net.createServer(socket => {
-    socket.on('data', chunk => {
-        const pageRequest = chunk.toString().split(' ')[1]
+const server = http.createServer((request, response) => {
+    const rs = fs.createReadStream(request.url === '/' ? './index.html' : `.${request.url}.html`)
 
-        const rs = fs.createReadStream(pageRequest === '/' ? './index.html' : `./${pageRequest}.html`)
-
-        rs.on('error', () => {
-            socket.end(`HTTP/1.1 404 NOT FOUND
-Content-Type: text/html
-
-<h1>404 - Not Found</h1>`)
-            })
+    rs.on('error', () => {
+        response.writeHead(404, {  'Content-Type': 'text/html'  })
+        response.end('<h1>Not found</h1>')
+    })
         
-        rs.on('data', chunk => {
-            socket.write(`HTTP/1.1 200
-Content-Type: text/html
-
-${chunk}`)
-        })
-
-        rs.on('end', () => socket.destroy())
+    rs.on('data', chunk => {
+        response.writeHead(200, {  'Content-Type': 'text/html'  })
+        response.end(chunk.toString())
     })
 })
 
