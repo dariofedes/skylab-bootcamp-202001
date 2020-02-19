cf8c55e5806730bba37d49329f27d8d348620d05
Darío Fernández <dariofedes@protonmail.com>
2020-02-19 18:20:32 +0100

    add extensions handling, now is not mandatory to specify .html i  the url

--- staff/dario-fernandez/web-server/server.js
+++ staff/dario-fernandez/web-server/server.js
@@ -2,8 +2,6 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+app.use('/', express.static(path.join(__dirname, 'public'), { extensions: ['html'] }))
 
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.listen('8080')
+app.listen('8081')
